e3b502be433d9b2f1e6aaca0f54c19d95d5d99e8
AbhilashBasumatary <abhilashbasumatary@gmail.com>
2020-05-24 21:33:35 -0700

    New login and signup

---
+++ app/src/main/java/com/abhi/firstscreen/lin.java
@@ -0,0 +1,14 @@
+package com.abhi.firstscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lin);
+    }
+}
---
+++ app/src/main/java/com/abhi/firstscreen/sup.java
@@ -0,0 +1,14 @@
+package com.abhi.firstscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sup);
+    }
+}
